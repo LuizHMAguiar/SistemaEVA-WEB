015e5b43acca94207b987c99b5565cc08c8849b2
LuizHMAguiar <1397749@etfbsb.edu.br>
2026-03-24 12:09:55 -0300

    feat: Área de conteúdo do dashboard.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { FormularioLogin } from './components/Login/FormularioLogin.tsx'
 import { FormularioCadastro } from './components/Cadastro/FormularioCadastro.tsx'
 import { BarraTitulo } from './components/Dashboard/BarraTitulo.tsx'
 import { Menu } from './components/Dashboard/Menu.tsx'
+import { Conteudo } from './components/Dashboard/Conteudo.tsx'
 
 
 
@@ -13,6 +14,7 @@ function App() {
     <>
       <BarraTitulo />
       <Menu />
+      <Conteudo />
     </>
   )
 }
---
+++ src/components/Dashboard/Conteudo.tsx
@@ -0,0 +1,93 @@
+import "./Conteudo.css"
+
+export function Conteudo(){
+    return (
+        <div className="Conteudo">
+            <div className="Avaliacao">
+                <div className="Cabecalho">
+                    <div className="TipoAvaliacao TipoProva">
+                    Prova
+                    </div>
+                    <div className="TituloProva">
+                        Provas de Algoritimos
+                    </div>
+                </div>
+                <div className="Disciplina">
+                    ProgramaçãoI - Turma2024-A
+                </div>
+                <div className="MaisDetalhesAvaliacao">
+                    <div className="Data">
+                    15/01-20/01/2024
+                    </div>
+                    <div className="Tempo">
+                        120 Min
+                    </div>
+                    <div className="Questoes">
+                        15 Questoes
+                    </div>
+                </div>
+                <div className="Botao">
+                    Iniciar Avaliação
+                </div>
+            </div>
+            <div className="Avaliacao">
+                <div className="Cabecalho">
+                    <div className="TipoAvaliacao TipoSimulado">
+                    Simulado
+                    </div>
+                    <div className="TituloProva">
+                        Simulado ENADE 2024
+                    </div>
+                </div>
+                <div className="Disciplina">
+                    ProgramaçãoI - Turma2024-A
+                </div>
+                <div className="MaisDetalhesAvaliacao">
+                    <div className="Data">
+                    15/01-20/01/2024
+                    </div>
+                    <div className="Tempo">
+                        120 Min
+                    </div>
+                    <div className="Questoes">
+                        20 Questoes
+                    </div>
+                </div>
+                <div className="Botao">
+                    Disponivel dia 29/08/2024
+                </div>           
+            
+            </div>
+
+            <div className="Avaliacao">
+                <div className="Cabecalho">
+                    <div className="TipoAvaliacao TipoSimulado">
+                    Simulado
+                    </div>
+                    <div className="TituloProva">
+                        Simulado ENADE 2024
+                    </div>
+                </div>
+                <div className="Disciplina">
+                    ProgramaçãoI - Turma2024-A
+                </div>
+                <div className="MaisDetalhesAvaliacao">
+                    <div className="Data">
+                    15/01-20/01/2024
+                    </div>
+                    <div className="Tempo">
+                        120 Min
+                    </div>
+                    <div className="Questoes">
+                        20 Questoes
+                    </div>
+                </div>
+                <div className="Botao">
+                    Disponivel dia 29/08/2024
+                </div>           
+            
+            </div>
+                
+        </div>
+        )
+}
