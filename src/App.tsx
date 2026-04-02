ef6130d6339b879573f1a2c059e5f0462bd2d7b6
LuizHMAguiar <1397749@etfbsb.edu.br>
2026-04-02 09:50:46 -0300

    fix: imports

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import { FormularioCadastro } from './components/Cadastro/FormularioCadastro.tsx
 import { BarraTitulo } from './components/Dashboard/BarraTitulo.tsx'
 import { Menu } from './components/Dashboard/Menu.tsx'
 import { Conteudo } from './components/Dashboard/Conteudo.tsx'
-import { Modal } from './components/Dashboard/Modal.tsx'*/
+import { Modal } from './components/Dashboard/Modal.tsx'
 import { BarraNavegacao} from './components/Avaliacao/BarraNavegacao.tsx'
 import { BotoesNavegacao } from './components/Avaliacao/BotoesNavegacao.tsx'
-import { Questao } from './components/Avaliacao/Questao.tsx'
+import { Questao } from './components/Avaliacao/Questao.tsx'*/
 import { FormularioLogin } from './components/Login/FormularioLogin.tsx'
 
 function App() {
