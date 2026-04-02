1feb2766f63fccb6743657a2708f98ece1c1ca44
LuizHMAguiar <1397749@etfbsb.edu.br>
2026-04-02 10:27:01 -0300

    fix: allowed hosts para servidor Render

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Allow preview to accept requests from this external host
+  preview: {
+    allowedHosts: ['sistemaeva.onrender.com/'],
+  },
 })
